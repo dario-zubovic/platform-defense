5ba0fe246fce61b63354720ec34f828eee119190
Dario <dario@zubovic.email>
2018-11-10 15:44:23 +0100

    And another travis bug

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,14 @@
 #! /bin/sh
 
 # butler
-curl https://broth.itch.ovh/butler/linux-amd64/LATEST/archive/default -o butler.zip
+echo "Downloading butler..."
+curl -o butler.zip https://broth.itch.ovh/butler/linux-amd64/LATEST/archive/default
+echo `md5sum butler.zip`
 unzip butler.zip -d ./
 chmod +x ./butler
 
 # unity
+echo "Downloading Unity..."
 curl https://beta.unity3d.com/download/dad990bf2728/UnitySetup-2018.2.7f1
 sudo chmod +x ./UnitySetup-2018.2.7f1
 echo y | ./UnitySetup-2018.2.7f1 --unattended --install-location=/opt/Unity/Editor --verbose --download-location=/tmp/unity --components=Unity,WebGL
