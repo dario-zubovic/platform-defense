03b39e8840c43267008a2ad7348e2de9fe9c9082
Dario <dario@zubovic.email>
2018-11-10 14:52:43 +0100

    Fixed travis

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,7 +8,7 @@ chmod +x ./butler
 # unity
 curl https://beta.unity3d.com/download/dad990bf2728/UnitySetup-2018.2.7f1
 sudo chmod +x ./UnitySetup-2018.2.7f1
-echo y | ./UnitySetup-2018.2.7f1--unattended --install-location=/opt/Unity/Editor --verbose --download-location=/tmp/unity --components=Unity,WebGL
+echo y | ./UnitySetup-2018.2.7f1 --unattended --install-location=/opt/Unity/Editor --verbose --download-location=/tmp/unity --components=Unity,WebGL
 mkdir -p $HOME/.local/share/unity3d/Certificates/
 
 # misc
