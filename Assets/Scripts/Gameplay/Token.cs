5f2b7bf98d6c88f7963e1de9ee2274e78ac371e5
Dario <dario@zubovic.email>
2018-11-14 15:28:44 +0100

    Token pickup

---
+++ Assets/Scripts/Gameplay/Token.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using UnityEngine;
+
+public class Token : MonoBehaviour {
+    public float weight;
+    public float delay;
+    public bool isStarting;
+    public bool isDropToken;
+
+    public bool isActive {
+        get;
+        private set;
+    }
+
+    public bool canBeActivated {
+        get {
+            if(this.isActive) {
+                return false;
+            }
+
+            return Time.time - this.lastPickupTime > this.delay;
+        }
+    }
+
+    private float lastPickupTime;
+
+    private SpriteRenderer rend;
+    private Level level;
+
+    public void Awake() {
+        this.rend = this.gameObject.GetComponent<SpriteRenderer>();
+        this.level = GameObject.FindObjectOfType<Level>();
+
+        if(this.isDropToken) {
+            this.lastPickupTime = Time.time;
+            Activate();
+            return;
+        }
+
+        if(this.isStarting) {
+            Activate();
+        } else {
+            Hide();
+            this.lastPickupTime = -1000;
+        }
+    }
+
+    public void Activate() {
+        this.isActive = true;
+        this.rend.enabled = true;
+    }
+
+    public void MoveTo(Vector3 pos) {
+        StartCoroutine(Move(this.transform.position, pos, 0.1f));
+    }
+
+    public void OnTriggerEnter2D(Collider2D coll) {
+        if(!this.isActive || (this.isStarting && Time.time - this.lastPickupTime < 2f)) {
+            return;
+        }
+
+        if(coll.gameObject.layer != LayerMask.NameToLayer("Player")) {
+            return;
+        }
+
+        this.lastPickupTime = Time.time;
+        Hide();
+        this.level.Pickup(this.isDropToken == false);
+    }
+
+    private IEnumerator Move(Vector3 start, Vector3 destination, float duration) {
+        float startTime = Time.time;
+        while(Time.time < startTime + duration) {
+            this.transform.position = Vector3.Lerp(start, destination, (Time.time - startTime) / duration);
+            yield return null;
+        }
+    }
+
+    private void Hide() {
+        this.isActive = false;
+        this.rend.enabled = false;
+    
+        if(this.isDropToken) {
+            GameObject.Destroy(this.gameObject); // TODO: pool
+        }
+    }
+}
--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -8,17 +8,28 @@ public class Level : MonoBehaviour {
 	public float dramaTime;
 	public float respawnTime;
 	public Bounds levelBounds;
+
+	[Header("Tokens")]
+	public LayerMask tokenCollisionMask;
 	
 	[Header("Prefabs")]
 	public Player playerPrefab;
+	public Token dropTokenPrefab;
 
 	private CameraController cameraController;
 	private Player player;
+
+	private int collectedTokens = 0;
+	private List<Token> tokens;
+	private List<Token> droppedTokens;
 
 	public void Awake() {
 		this.cameraController = Camera.main.GetComponent<CameraController>();
 
 		this.player = GameObject.Instantiate<Player>(this.playerPrefab);
+
+		this.tokens = new List<Token>(GameObject.FindObjectsOfType<Token>());
+		this.droppedTokens = new List<Token>();
 	}
 
 	public void Start() {
@@ -28,7 +39,17 @@ public class Level : MonoBehaviour {
 	}
 
 	public void PlayerDied() {
+		DropTokens();
+
 		StartCoroutine(WaitForRespawn());
+	}
+
+	public void Pickup(bool spawnNew) {
+		this.collectedTokens++;
+
+		if(spawnNew) {
+			StartCoroutine(SpawnNewToken());
+		}
 	}
 
 	private IEnumerator WaitForRespawn() {
@@ -47,6 +68,91 @@ public class Level : MonoBehaviour {
 
 		this.player.Spawn(this.respawn.position);
 		this.cameraController.SetTarget(this.player.transform, false);
+	}
+
+	private IEnumerator SpawnNewToken() {
+		yield return new WaitForSeconds(5f);
+
+		Token selected = null;
+		List<Token> availableTokens = new List<Token>(this.tokens.Count);
+		float totalWeights = 0;
+
+		while(selected == null) {
+			totalWeights = 0;
+			availableTokens.Clear();
+
+			foreach(var token in this.tokens) {
+				if(!token.canBeActivated) {
+					continue;
+				}
+
+				availableTokens.Add(token);
+				totalWeights += token.weight;
+			}
+
+			if(availableTokens.Count == 0) {
+				yield return new WaitForSeconds(0.5f);
+				continue;
+			}
+
+			float r = Random.value * totalWeights;
+			float i = 0;
+			foreach(var token in availableTokens) {
+				if(r > i && r <= i + token.weight) {
+					selected = token;
+					break;
+				} else {
+					i += token.weight;
+				}
+			}
+		}
+
+		selected.Activate();
+	}
+
+	private void DropTokens() {
+		foreach(var token in this.droppedTokens) {
+			if(token == null || token.gameObject == null) {
+				continue;
+			}
+
+			GameObject.Destroy(token.gameObject); // TODO: pool
+		}
+		this.droppedTokens.Clear();
+
+		int leftovers = Mathf.CeilToInt(this.collectedTokens / 2f);
+		this.collectedTokens = 0;
+
+		for(int i = 0; i < leftovers; i++) {
+			Token token = GameObject.Instantiate<Token>(this.dropTokenPrefab, this.player.transform.position, Quaternion.identity); // TODO: pool
+			
+			Vector2 safePos = FindDropTokenSafeLocation(token);
+			token.transform.position = this.player.transform.position + Vector3.up;
+			token.MoveTo(safePos);
+
+			this.droppedTokens.Add(token);
+		}
+	}
+
+	private Vector2 FindDropTokenSafeLocation(Token token) {
+		Collider2D coll = token.gameObject.GetComponent<Collider2D>();
+		ContactFilter2D filter2D = new ContactFilter2D();
+		filter2D.layerMask = this.tokenCollisionMask;
+		Collider2D[] results = new Collider2D[8];
+
+		Vector2 startPos = token.transform.position;
+
+		for(int i = 0; i < 64; i++) {
+			Vector2 pos = startPos + Random.insideUnitCircle * 6f;
+			token.transform.position = pos;
+
+			int c = Physics2D.OverlapCollider(coll, filter2D, results);
+			if(c == 0) {
+				return pos;
+			}
+		}
+
+		return startPos;
 	}
 
 #if UNITY_EDITOR
